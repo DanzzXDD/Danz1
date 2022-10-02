7ba2d6f3760304b3793b6cbd588391a5861e2e49
PemburuGrat1san <danishganzz.gmail.com>
2022-10-02 17:18:01 +0700

    Danz1

---
+++ kitzuna.js
@@ -0,0 +1,16 @@
+//━━━━━━━━━━━━━━━[ PERINGATAN ]━━━━━━━━━━━━━━━━━//
+Hai Gw Selaku Pembuat Nih Sc Berterima Kasih
+ Kepada:
+
+あ DikaArdnt [Author]
+あ DaniUhuy [Newbie]
+あ Manik [Mastah]
+あ Fatih Arridho [Sensei]
+あ Penyedia Module 
+あ Penyedia Rest Api
+
+//━━━━━━━━━━━━━━━[ AKHIR ]━━━━━━━━━━━━━━━━━//
+Oke Makasih Banyak Yang Udah Support
+Channel YouTube Gw Dan Jangan Lupa 
+Like, Subscribe And Share 
+//------------------------------------------GABUTZ-BANG-----------------------------------------//
